295f32844f974fc12802805b5c2817e5ae087538
briitogabriel <gabriel.brito.a@hotmail.com>
2023-10-17 20:29:52 -0300

    testando cadastro de dieta com login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { useState } from 'react';
 
+// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBhc3luY2xhYi5jb20iLCJpYXQiOjE2OTc1ODQxNDYsImV4cCI6MTY5NzU4Nzc0Nn0.MD4YrcYNOxiSp0FCO1ewYkVf-fz0PyDdeJiGGMNukB4'
+
 export const App = () => {
-  let initialState = {
+  let initialUsuario = {
+    email: "admin@asynclab.com",
+	  senha: "admin123"
+  }
+  let initialDieta = {
     die_nome: "Dieta da lua",
     die_data: "2023-10-01",
     die_hora: "00:00:00",
@@ -11,15 +17,19 @@ export const App = () => {
     pac_id: 1
   }
     const inputStyle = {border: "1px solid black", height: 75, "padding": 10}
-    const [dieta, setDieta] = useState(initialState)
+    const [dieta, setDieta] = useState(initialDieta)
+    const [usuario, setUsuario] = useState(initialUsuario)
     const [response, setResponse] = useState()
 
-    const sumbitForm = (e) => {
+    const [token, setToken] = useState('')
+
+    const sumbitUsuario = (e) => {
       e.preventDefault()
-      axios.post('http://localhost:3333/api/dietas', dieta)
+      axios.post('http://localhost:3333/api/usuarios/login', usuario)
       .then((res) => {
+        setToken(res.data.data)
         setResponse(res.data)
-        setDieta(initialState)
+        setUsuario(initialUsuario)
         console.log(res.data)
       })
       .catch(function (error) {
@@ -28,24 +38,63 @@ export const App = () => {
     }
     const onChangeHandler = (event) => {
       const {name, value} = event
+        setUsuario((prev) => {
+          return {...prev, [name]: value}
+        })
+    }
+
+    const sumbitDieta = (e) => {
+      e.preventDefault()
+      console.log(token)
+      axios.post('http://localhost:3333/api/dietas', dieta, {
+        headers: {
+          'Authorization': `${token}`
+        }
+      })
+      .then((res) => {
+        setDieta(initialDieta)
+        setResponse(res.data)
+        console.log(res.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+    const onChangeDieta = (event) => {
+      const {name, value} = event
         setDieta((prev) => {
           return {...prev, [name]: value}
         })
     }
-     return(
-          <form onSubmit={sumbitForm}>
+
+      return(
+        <>
+          <form onSubmit={sumbitUsuario}>
             <table style={inputStyle}>
               <tbody>
-                <tr><td>Nome:</td><td><input type="text" name="die_nome" value={dieta.die_nome} onChange={(e) => onChangeHandler(e.target)}/></td></tr>
-                <tr><td>Data:</td><td><input type="date" name="die_data" value={dieta.die_data} onChange={(e) => onChangeHandler(e.target)}/></td></tr>
-                <tr><td>Hora:</td><td><input type="time" name="die_hora" value={dieta.die_hora} onChange={(e) => onChangeHandler(e.target)}/></td></tr>
-                <tr><td>Tipo:</td><td><input type="text" name="die_tipo" value={dieta.die_tipo} onChange={(e) => onChangeHandler(e.target)}/></td></tr>
-                <tr><td>Descrição:</td><td><input type="text" name="die_descricao" value={dieta.die_descricao} onChange={(e) => onChangeHandler(e.target)}/></td></tr>
+                <tr><td>Email:</td><td><input type="text" name="email" value={usuario.email} onChange={(e) => onChangeHandler(e.target)}/></td></tr>
+                <tr><td>Senha:</td><td><input type="text" name="senha" value={usuario.senha} onChange={(e) => onChangeHandler(e.target)}/></td></tr>
                 <tr><td ><button type="submit">Submit</button></td></tr>
                 {response?.response && <tr><td colSpan={2}>{response.response}</td></tr>}
               </tbody>
             </table>
           </form>
-     )
+
+          <form onSubmit={sumbitDieta}>
+            <table style={inputStyle}>
+              <tbody>
+                <tr><td>Nome:</td><td><input type="text" name="die_nome" value={dieta.die_nome} onChange={(e) => onChangeDieta(e.target)}/></td></tr>
+                <tr><td>Data:</td><td><input type="date" name="die_data" value={dieta.die_data} onChange={(e) => onChangeDieta(e.target)}/></td></tr>
+                <tr><td>Hora:</td><td><input type="time" name="die_hora" value={dieta.die_hora} onChange={(e) => onChangeDieta(e.target)}/></td></tr>
+                <tr><td>Tipo:</td><td><input type="text" name="die_tipo" value={dieta.die_tipo} onChange={(e) => onChangeDieta(e.target)}/></td></tr>
+                <tr><td>Descrição:</td><td><input type="text" name="die_descricao" value={dieta.die_descricao} onChange={(e) => onChangeDieta(e.target)}/></td></tr>
+                <tr><td ><button type="submit">Submit</button></td></tr>
+                {response?.response && <tr><td colSpan={2}>{response.response}</td></tr>}
+              </tbody>
+            </table>
+          </form>
+        
+        </>
+      )
    }
 export default App;
