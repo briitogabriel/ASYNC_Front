fc4bd4ebb32206e1be076f7d59b1ed60c819f232
AnjaClara <anaclaraguimaraes1@gmail.com>
2023-10-25 22:08:24 -0300

    esqueleto pagina de login

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { DietaPage } from "../pages/Dieta/Dieta.page.jsx";
 import Usuario from "../pages/Usuario/Usuario.page";
+import Login from "../pages/Login/Login.page.jsx";
 
 export const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRoutes = () => {
         <Route path="/usuarios/login" element={<p>Login page</p>}/>
         <Route path="/">  {/*REMOVI POIS NÃO ESTAVA ENTRANDO NO ENDPOINT ->>> element={<p>Layout</p>}>*/}
           <Route index element={<p>Página inicial</p>}/>
+          <Route path="/login" element={<Login/>}/>
           <Route path="/cadastrar-dieta" element={<DietaPage />} />
           <Route path="/cadastrar-usuario" element={<Usuario />} />
         </Route>
