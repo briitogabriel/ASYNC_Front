205c8462ea6ffc20a1385ccd3bcd261832360864
briitogabriel <gabriel.brito.a@hotmail.com>
2023-10-25 21:11:44 -0300

    mock API para buscar pacientes

--- src/services/Dieta.service.jsx
+++ src/services/Dieta.service.jsx
@@ -16,6 +16,19 @@ const criarDieta = (dieta) => {
   });
 };
 
+// SOMENTE MOCK DE PACIENTES NO SERVICE DE DIETAS -> APAGAR DEPOIS
+const buscarPacientes = () => {
+  return axios.get('http://localhost:3333/api/dietas/pacientes')
+  .then((res) => {
+    return res.data
+  })
+  .catch((error) => {
+    console.log(error.response.data)
+    return false
+  });
+};
+
 export const DietaService = {
   criarDieta,
+  buscarPacientes,
 };
