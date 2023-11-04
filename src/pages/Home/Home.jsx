9e04dcecc821016c156d443cb8e8af4ac8b3a833
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-11-04 13:58:05 -0300

    iniciando página de home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,35 @@
-import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/MenuLateral/Navbar/Navbar';
 import { useEffect, useState } from "react";
 import { CardEstatistica } from "../../components/CardEstatistica/CardEstatistica.component";
 import { PacienteService } from "../../services/Paciente.service";
+import { ConsultaService } from '../../services/Consultas.service';
 
-export const Home = () => {
+export const HomePage = () => {
     const [estatistica, setEstatistica] = useState({});
   
-    const calcularEstatistica = async () => {
-        const pacientes = await PacienteService.buscarPacientes();
+    useEffect( () => {
+        const calcularEstatistica = async () => {
+            const pacientes = await PacienteService.buscarPacientes();
+            const qtdPacientes = pacientes.lenght;
 
-        setEstatistica({pacientes: pacientes.lengh})
-    }
+            const consultas = await ConsultaService.listarConsultas();
+            const qtdConsultas = consultas.lenght;
+
+            setEstatistica({pacientes: qtdPacientes, cosultas: qtdConsultas, exames: 0});
+        };
+         calcularEstatistica();
+        
+    }, []);
 
     return <div>
         <Navbar/>
-        <div className="mt-3">
-            <h4>Home</h4>
-            <div className="row"></div>
+        <div className="container mt-3">
+            <h4>Estatísticas do sistema</h4>
+            <div className="row mt-3">
+                <CardEstatistica icon='bi-person-circle' value={estatistica.pacientes} label='Pacientes' />
+                <CardEstatistica icon='bi-heart-pulse' value={estatistica.pacientes} label='Consultas' />
+                <CardEstatistica icon='bi-journal-text' value={estatistica.pacientes} label='Exames' />
+            </div>
         </div>
     </div>;
 };
