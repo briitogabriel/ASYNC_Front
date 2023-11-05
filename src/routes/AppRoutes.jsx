2252837059b2243e114ca72a2a221721ab927549
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-11-05 13:42:18 -0300

    fix bug rotas privadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,18 @@
-import React, {useState} from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Dietas from "../pages/Dietas/Dietas.page.jsx";
-import Exercicios from '../pages/Exercicios/Exercicios.page.jsx';
 import Login from '../pages/Login/Login.page.jsx';
 import Exames from '../pages/Exames/Exames.page.jsx';
-import Usuario from "../pages/Usuario/Usuario.page";
-import DetalhaProntuario from "../pages/DetalhaProntuario/DetalhaProntuario.page";
+import Usuario from "../pages/Usuario/Usuario.page.jsx";
+import DetalhaProntuario from "../pages/DetalhaProntuario/DetalhaProntuario.page.jsx";
 import Prontuarios from '../pages/Prontuarios/Prontuarios.page.jsx';
-import { ToastProvider } from '../contexts/ToastContext';
+import { ToastProvider } from '../contexts/ToastContext.jsx';
 import Navbar from '../components/MenuLateral/Navbar/Navbar.jsx';
 import Toolbar from '../components/Toolbar/Toolbar.page.jsx';
 import { PacientesPage } from '../pages/Pacientes/Pacientes.page.jsx';
 import { MedicamentosPage } from '../pages/Medicamentos/Medicamentos.page.jsx';
 import { ConsultaPage } from '../pages/Consulta/Consulta.page.jsx';
 import { HomePage } from '../pages/Home/Home.jsx';
+import { PrivateRoutes } from './PrivateRoutes.jsx';
 
 export const AppRoutes = () => {
 
@@ -22,23 +21,71 @@ export const AppRoutes = () => {
       <Routes>               
         <Route path="/usuarios/login" element={<Login />} />
         <Route path="/">
-          <Route index element={<HomePage />} />
-          <Route path="/lateralmenu" element={<Navbar/>}/>
-          <Route path="/toolbar" element={<Toolbar/>}/>
-          <Route path="/dietas" element={<Dietas />} />
-          <Route path="/pacientes/:idPaciente/dietas/:idDieta" element={<Dietas />} />
-          <Route path="/exercicios" element={<Exercicios />} />
-          <Route path="/pacientes/:idPaciente/exercicios/:idExercicio" element={<Exercicios />} />
-          <Route path="/cadastrar-usuario" element={<Usuario />} />
-          <Route path="/prontuarios" element={<Prontuarios />} />
-          <Route path="/prontuarios/:idPaciente" element={<DetalhaProntuario />} />
-          <Route path="/exames" element={<Exames />} />
-          <Route path="/pacientes/:idPaciente/exames" element={<Exames />} />
-          <Route path="/pacientes/:idPaciente/exames/:id" element={<Exames />} />
-          <Route path="/pacientes" element={<PacientesPage />} />
-          <Route path="/medicamentos" element={<MedicamentosPage />}/>
-          <Route path="pacientes/:pacienteId/medicamentos" element={<MedicamentosPage />}/>
-          <Route path="/consultas" element={<ConsultaPage />} />
+            <Route index element={
+              <PrivateRoutes>
+                <HomePage />
+              </PrivateRoutes>
+            } />
+          <Route path="/lateralmenu" element={
+            <PrivateRoutes>
+              <Navbar/>
+            </PrivateRoutes>
+          }/>
+          <Route path="/toolbar" element={
+            <PrivateRoutes>
+              <Toolbar/>
+            </PrivateRoutes>
+          }/>
+          <Route path="/cadastrar-dieta" element={
+            <PrivateRoutes>
+              <Dietas />
+            </PrivateRoutes>
+          } />
+          <Route path="/cadastrar-usuario" element={
+            <PrivateRoutes>
+              <Usuario />
+            </PrivateRoutes>
+          } />
+          <Route path="/prontuarios" element={
+            <PrivateRoutes>
+              <Prontuarios />
+            </PrivateRoutes>
+          } />
+          <Route path="/prontuarios/:idPaciente" element={
+            <PrivateRoutes>
+              <DetalhaProntuario />
+            </PrivateRoutes>
+          } />
+          <Route path="/exames" element={
+            <PrivateRoutes>
+              <Exames />
+            </PrivateRoutes>
+          } />
+          <Route path="/pacientes/:idPaciente/exames" element={
+            <PrivateRoutes>
+              <Exames />
+            </PrivateRoutes>
+          } />
+          <Route path="/pacientes/:idPaciente/exames/:id" element={
+            <PrivateRoutes>
+              <Exames />
+            </PrivateRoutes>} />
+          <Route path="/pacientes" element={
+            <PrivateRoutes>
+              <PacientesPage />
+            </PrivateRoutes>} />
+          <Route path="/medicamentos" element={
+            <PrivateRoutes>
+              <MedicamentosPage />
+            </PrivateRoutes>}/>
+          <Route path="pacientes/:pacienteId/medicamentos" element={
+            <PrivateRoutes>
+              <MedicamentosPage />
+            </PrivateRoutes>}/>
+          <Route path="/consultas" element={
+            <PrivateRoutes>
+              <ConsultaPage />
+            </PrivateRoutes>} />
         </Route>
         <Route path="*" element={<p>Página não existe</p>} />
       </Routes>
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 import { LocalStorageService } from '../services/LocalStorage.service';
+import { AuthContext } from '../contexts/auth.context';
+import { useContext } from 'react';
 
 export const PrivateRoutes = ({children}) => {
-    return LocalStorageService.get() ? children : <Navigate to="usuarios/login"/>
+    const {auth} = useContext(AuthContext);
+    return auth.isLogged === true ? children : <Navigate to="/usuarios/login"/>
 };
 
 PrivateRoutes.propTypes = {
