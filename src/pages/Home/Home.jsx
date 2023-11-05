cfb03054a589194a5a430b76ea204208dff3c92c
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-11-05 12:35:00 -0300

    estatisticas de usuarios + style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,36 +6,73 @@ import { ConsultaService } from "../../services/Consultas.service";
 import { ExameService } from "../../services/Exames.service";
 import { CardPaciente } from "../../components/CardPaciente/CardPaciente";
 import { useForm } from "react-hook-form";
+import "./Home.style.css";
+import { UserService } from "../../services/Usuario.service";
 
 export const HomePage = () => {
   const { register, handleSubmit, reset } = useForm();
 
+  const [qtdUsuarios, setQtdUsuarios] = useState({
+    admins: 0,
+    medicos: 0,
+    enfermeiros: 0,
+  });
   const [estatistica, setEstatistica] = useState({
     pacientes: 0,
     consultas: 0,
     exames: 0,
   });
+  const [usuarios, setUsuarios] = useState([]);
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState();
 
+  const calcularUsuarios = async () => {
+    const usuariosDb = await UserService.getUsers();
+    setUsuarios(usuarios);
+    let qtdAdmins = 0;
+    let qtdMedicos = 0;
+    let qtdEnfermeiros = 0;
+
+    usuariosDb.map((usuario) => {
+      if (usuario.permissao === 1) {
+        qtdAdmins += 1;
+      } else if (usuario.permissao === 2) {
+        qtdMedicos += 1;
+      } else if (usuario.permissao === 3) {
+        qtdEnfermeiros += 1;
+      } else {
+        return;
+      }
+    });
+
+    setQtdUsuarios({
+      admins: qtdAdmins,
+      medicos: qtdMedicos,
+      enfermeiros: qtdEnfermeiros,
+    });
+  };
+
+  const calcularEstatistica = async () => {
+    const pacientes = await PacienteService.buscarPacientes();
+    setPacientes(pacientes);
+    const qtdPacientes = pacientes.length;
+
+    const consultas = await ConsultaService.listarConsultas();
+    const qtdConsultas = consultas.length;
+
+    const exames = await ExameService.listarExames();
+    const qtdExames = exames.length;
+
+    setEstatistica({
+      ...estatistica,
+      pacientes: qtdPacientes,
+      consultas: qtdConsultas,
+      exames: qtdExames,
+    });
+  };
+
   useEffect(() => {
-    const calcularEstatistica = async () => {
-      const pacientes = await PacienteService.buscarPacientes();
-      setPacientes(pacientes);
-      const qtdPacientes = pacientes.length;
-
-      const consultas = await ConsultaService.listarConsultas();
-      const qtdConsultas = consultas.length;
-
-      const exames = await ExameService.listarExames();
-      const qtdExames = exames.length;
-
-      setEstatistica({
-        pacientes: qtdPacientes,
-        consultas: qtdConsultas,
-        exames: qtdExames,
-      });
-    };
+    calcularUsuarios();
     calcularEstatistica();
   }, []);
 
@@ -47,7 +84,7 @@ export const HomePage = () => {
           data.info === paciente.pac_telefone ||
           data.info === paciente.pac_email
       );
-      
+
       if (!paciente) {
         throw new Error("Falha ao buscar paciente.");
       }
@@ -62,26 +99,29 @@ export const HomePage = () => {
     <div>
       <Navbar />
       <div className="container mt-3">
-      <h4>Estatísticas de usuários</h4>
-        <div className="row mt-3">
-          <CardEstatistica
-            icon="bi-person-circle"
-            value={1}
-            label="Administradores"
-          />
-          <CardEstatistica
-            icon="bi-heart-pulse"
-            value={1}
-            label="Médicos"
-          />
-          <CardEstatistica
-            icon="bi-journal-text"
-            value={1}
-            label="Enfermeiros"
-          />
+        {/* {usuarios.forEach((usuario) => usuario.permissao === 1 &&)} */}
+          <h3 className="text-center title">Estatísticas de usuários</h3>
+            <div className="row mt-3 justify-content-evenly">
+              <CardEstatistica
+                icon="bi-person-gear"
+                value={qtdUsuarios.admins}
+                label="Administradores"
+              />
+              <CardEstatistica
+                icon="bi-person"
+                value={qtdUsuarios.medicos}
+                label="Médicos"
+              />
+              <CardEstatistica
+                icon="bi-person"
+                value={qtdUsuarios.enfermeiros}
+                label="Enfermeiros"
+              />
+            </div>
+        <div className="mt-3">
+          <h3 className="text-center title">Estatísticas do sistema</h3>
         </div>
-        <h4>Estatísticas do sistema</h4>
-        <div className="row mt-3">
+        <div className="row mt-3 justify-content-evenly">
           <CardEstatistica
             icon="bi-person-circle"
             value={estatistica.pacientes}
@@ -99,7 +139,7 @@ export const HomePage = () => {
           />
         </div>
         <div className="mt-4">
-          <h4>Informações de pacientes</h4>
+          <h3 className="title">Informações de pacientes</h3>
           <form onSubmit={handleSubmit(submitForm)} className="mt-3">
             <div className="input-group mb-3">
               <input
@@ -107,7 +147,7 @@ export const HomePage = () => {
                 name="info"
                 id="info"
                 className="form-control"
-                placeholder="Digite o nome, telefone ou e-mail do paciente"
+                placeholder="Digite o nome completo, telefone ou e-mail do paciente"
                 aria-label="Digite o nome do paciente"
                 aria-describedby="button-addon2"
                 {...register("info")}
@@ -121,7 +161,7 @@ export const HomePage = () => {
               </button>
             </div>
           </form>
-          <div className="row">
+          <div className="row gap-1 justify-content-center">
             {!paciente ? (
               pacientes.map((paciente) => (
                 <CardPaciente
