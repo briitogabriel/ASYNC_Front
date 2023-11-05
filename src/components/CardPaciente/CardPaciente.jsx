b386981df3debf83c0ef2bef3fda48e2f158a116
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-11-05 10:37:14 -0300

    calculando idade do paciente

--- src/components/CardPaciente/CardPaciente.jsx
+++ src/components/CardPaciente/CardPaciente.jsx
@@ -2,13 +2,27 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export const CardPaciente = ({className, paciente}) => {
+
+    const getAge = () => {
+        const today = new Date();
+        const birthDate = new Date(paciente.pac_nascimento);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const m = today.getMonth() - birthDate.getMonth();
+    
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+        }
+
+        return age;
+    };
+
     return (
         <div className={className}>
             <div className="card">
                 <div className="card-body">
                     <h4 className="card-title"><i className={`fs-1 bi bi-person-circle`}></i> {paciente.pac_nome}</h4>
                     <div className="mt-4 ">
-                        <p><span className="fw-bold">Idade: </span> {12}</p>
+                        <p><span className="fw-bold">Idade: </span> {getAge()}</p>
                         <p><span className="fw-bold">Convênio: </span> {paciente.pac_convenio}</p>
                         <p><span className="fw-bold">Telefone: </span> {paciente.pac_telefone}</p>
                         <div className="text-center">
