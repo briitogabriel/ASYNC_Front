e0458c0129adbc2d26a7774e2235d674f72c6d2d
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-11-05 13:30:14 -0300

    implementando rotas privadas + corrigindo navigate

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { LocalStorageService } from '../services/LocalStorage.service';
 
 export const PrivateRoutes = ({children}) => {
-    return LocalStorageService.get() ? children : <Navigate to="usuarios/login"/>
+    return LocalStorageService.get() ? children : <Navigate to="/usuarios/login"/>
 };
 
 PrivateRoutes.propTypes = {
