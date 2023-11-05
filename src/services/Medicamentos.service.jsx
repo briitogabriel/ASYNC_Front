c4822012b2552d3ffa6955505ea1cf9225b5807b
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-11-05 16:44:15 -0300

    criando serviço para listar medicamentos

--- src/services/Medicamentos.service.jsx
+++ src/services/Medicamentos.service.jsx
@@ -63,9 +63,25 @@ const deletarMedicamento = (id) => {
     })
 };
 
+const listarMedicamentos = () => {
+    return axios.get(`http://localhost:${import.meta.env.VITE_APP_PORT}/api/medicamentos`, {
+        headers: {
+            'Authorization': LocalStorageService.get('token')
+          }
+    })
+    .then((res) => {
+        return res.data
+      })
+      .catch((error) => {
+        console.error(error.response.data)
+        throw new Error("Erro ao detalhar dados do medicamento");
+      });
+}
+
 export const MedicamentoService = {
     salvarMedicamento,
     detalharMedicamento,
     atualizarMedicamento,
     deletarMedicamento,
+    listarMedicamentos
 };
